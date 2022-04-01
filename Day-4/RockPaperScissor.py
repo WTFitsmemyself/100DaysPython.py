8d494a619e437c710e8989b36b43ff8cfee262a5
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-01 22:09:22 +0430

    --- Auto-Git Commit ---

--- Day-4/RockPaperScissor.py
+++ Day-4/RockPaperScissor.py
@@ -45,3 +45,27 @@ if pc_choice == Selection:
         print(f"You choose: \n{scissors}")
         print(f"Pc choose: \n{scissors}")
     print("Result is Draw")
+elif Selection == 0 and pc_choice == 1:
+    print(f"You choose: \n{rock}")
+    print(f"Pc choose: \n{paper}")
+    print("PC win")
+elif Selection == 0 and pc_choice == 2:
+    print(f"You choose: \n{rock}")
+    print(f"Pc choose: \n{scissors}")
+    print("You win")
+elif Selection == 1 and pc_choice == 0:
+    print(f"You choose: \n{paper}")
+    print(f"Pc choose: \n{rock}")
+    print("You win")
+elif Selection == 1 and pc_choice == 2:
+    print(f"You choose: \n{paper}")
+    print(f"Pc choose: \n{scissors}")
+    print("PC win")
+elif Selection == 2 and pc_choice == 0:
+    print(f"You choose: \n{scissors}")
+    print(f"Pc choose: \n{rock}")
+    print("PC win")
+elif Selection == 2 and pc_choice == 1:
+    print(f"You choose: \n{scissors}")
+    print(f"Pc choose: \n{paper}")
+    print("You win")
