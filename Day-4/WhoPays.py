038fd7c5e370dd909fafe54ce13f2dc01db64ced
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-02 02:40:23 +0430

    --- Auto-Git Commit ---

--- Day-4/WhoPays.py
+++ Day-4/WhoPays.py
@@ -3,6 +3,6 @@ import random
 names = input("Please enter some names (Jack,Nichole,...): ")
 name_list = names.split(",")
 len_list = len(name_list)
-random_index = random.randint(0,len_list)
+random_index = random.randint(0,len_list-1)
 
 print(f"{name_list[random_index]} is going to pay for dinner")
