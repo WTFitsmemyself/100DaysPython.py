9c9b576cb8dfd2aa79c74999820f825f1f8c9a41
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-01 23:09:35 +0430

    --- Auto-Git Commit ---

---
+++ Day-4/HeadOrTail.py
@@ -0,0 +1,6 @@
+import random
+random1or0 = random.randint(0,1)
+if random1or0 == 0:
+    print("Tails")
+else:
+    print("Heads")
---
+++ Day-4/RandomDecimalNumber.py
@@ -0,0 +1,5 @@
+import random 
+randint = random.randint(0,5)
+randDecimal = random.random()
+randDecimalFinall = randint - randDecimal
+print(randDecimalFinall)
