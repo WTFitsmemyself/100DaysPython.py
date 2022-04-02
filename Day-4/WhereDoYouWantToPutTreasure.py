f9b90dfb3c13e5ab0872ebe95e376302fa367a3d
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-02 13:23:11 +0430

    --- Auto-Git Commit ---

--- Day-4/WhereDoYouWantToPutTreasure.py
+++ Day-4/WhereDoYouWantToPutTreasure.py
@@ -3,8 +3,13 @@ row2 = ["🗳️","🗳️","🗳️"]
 row3 = ["🗳️","🗳️","🗳️"]
 
 map = [row1, row2, row3]
-print(f"{row1}\n {row2}\n {row3}\n")
-selection = input("Where do you want to put treasure(31 = row 3 colum 1): ")
+print(f" {row1}\n {row2}\n {row3}\n")
+selection = input("Where do you want to put treasure(3 1 = row 3 colum 1): ")
 
 onebyone=selection.split()
-print(onebyone)
+int_row = int(onebyone[0]) - 1
+int_col = int(onebyone[1]) - 1
+
+map[int_row][int_col] = "x"
+
+print(f" {row1}\n {row2}\n {row3}\n")
