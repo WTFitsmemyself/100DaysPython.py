97e2f3539ac81e76d285e52332e56a617667a64a
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-02 16:53:56 +0430

    --- Auto-Git Commit ---

--- Day-5/AverageHeight.py
+++ Day-5/AverageHeight.py
@@ -1,8 +1,6 @@
 student_heights = input("Input a list of student heights (seperated by space): ").split()
 
 for i in range(0, len(student_heights)):
-    student_heights[i] = int(student_heights[i])
-print(type(student_heights))
-average = (sum(student_heights))/(len(student_heights))
-print(type(average))
-print(average)
+    student_heights[i] = int(student_heights[i]) #type:ignore
+average = (sum(student_heights))/(len(student_heights)) #type:ignore
+print(round(average))
