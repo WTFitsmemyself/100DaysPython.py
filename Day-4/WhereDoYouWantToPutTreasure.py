3c2e7570648b8034f3c9a489e9114f339a095096
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-02 12:22:57 +0430

    --- Auto-Git Commit ---

--- Day-4/WhereDoYouWantToPutTreasure.py
+++ Day-4/WhereDoYouWantToPutTreasure.py
@@ -1,3 +1,10 @@
 row1 = ["🗳️","🗳️","🗳️"]
 row2 = ["🗳️","🗳️","🗳️"]
 row3 = ["🗳️","🗳️","🗳️"]
+
+map = [row1, row2, row3]
+print(f"{row1}\n {row2}\n {row3}\n")
+selection = input("Where do you want to put treasure(31 = row 3 colum 1): ")
+
+onebyone=selection.split()
+print(onebyone)
