6eb56167dc45ce7c6eaa984e3f4329551770570e
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-04-10 15:03:35 +0430

    Update PasswordGenerator.py

--- Day-5/PasswordGenerator.py
+++ Day-5/PasswordGenerator.py
@@ -1,23 +1,36 @@
+from ast import Pass
 import random
+import sys
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+Password = []
 
 print("Welcome to Password Generator!")
 HowManyLetters = input("How many letters would you like in your Password?: ")
 HowManySymbols = input("How many symbols would you like in your Password?: ")
 HowManyNumbers = input("How many numbers would you like in your Password?: ")
 
-for i in range(1,int(HowManyLetters)):
-    randomics = random.choices(letters,None)
-    print(f"{randomics}")
-            
+def ListToString(list):
+    string = ""
+    for i in range(len(list)):
+        string += list[i]
+    return string
 
+for i in range(0,int(HowManyLetters)):
+    randomics = random.choice(letters)
+    Password.append(randomics)
 
-# print(f"Here is your Password: {Password}")
+for i in range(0,int(HowManySymbols)):
+    randomics = random.choice(symbols)
+    Password.append(randomics)
 
+for i in range(0,int(HowManyNumbers)):
+    randomics = random.choice(numbers)
+    Password.append(randomics)
+    
+random.shuffle(Password)
+Password_String = ListToString(Password)
 
-
-# if HowManySymbols != 0:
-#     for i in range(1,int(HowManySymbols)):
+print(f"Your Password is: {Password_String}")
