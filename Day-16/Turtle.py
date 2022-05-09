0d19dda047a01d8126d40f53b8b5bb277cbb888b
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-05-09 18:04:43 +0430

    Day added

---
+++ Day-16/Turtle.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+import prettytable
+
+lakPosht = Turtle()
+lakPosht.shape("turtle")
+lakPosht.color("DarkViolet")
+myScreen = Screen()
+lakPosht.forward(100)
+myScreen.exitonclick()
