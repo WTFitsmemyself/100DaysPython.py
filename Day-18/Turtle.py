38e2212c63a6d4da5df2166545c1032c133ab391
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-05-16 10:51:13 +0430

    Day-18 updated

---
+++ Day-18/Turtle.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+Tim = Turtle()
+Tim.shape("turtle")
+Tim.color("#994184")
+screen = Screen()
+while True:
+    Tim.forward(100)
+    Tim.right(90)
+    
+screen.exitonclick()
