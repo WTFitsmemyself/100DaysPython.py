5c27719c02e48e3c4430f727695a1d78f1a9fafa
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-05-24 15:13:12 +0430

    Day-19 final added

---
+++ Day-19/turtleRace.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+from random import randint
+
+screen = Screen()
+screen.setup(height=500, width=500)
+screen.bgcolor("black")
+colors = ["red", "green", "blue", "yellow", "magenta", "cyan"]
+x_position = -230
+y_positions = [70, 40, 10, -20, -50, -80]
+all_turtles = []
+match_is_on = False
+
+
+def turtle_settle():
+    for turtles_in in range(0, 6):
+        tim = Turtle()
+        tim.shape("turtle")
+        tim.penup()
+        tim.color(colors[turtles_in])
+        tim.goto(x=x_position, y=y_positions[turtles_in])
+        all_turtles.append(tim)
+
+
+user_bet = screen.textinput("Who will win?", "Please insert the color you think will win:")
+
+if user_bet:
+    match_is_on = True
+turtle_settle()
+
+
+while match_is_on:
+    for turtles in all_turtles:
+        if turtles.xcor() > 230:
+            winner = turtles.pencolor()
+            if winner == user_bet:
+                print("You Win")
+            else:
+                print(f"Your Loose color {winner} is winner")
+            match_is_on = False
+
+        rand_distance = randint(0, 10)
+        turtles.forward(rand_distance)
+
+screen.exitonclick()
--- Day-5/PasswordGenerator.py
+++ Day-5/PasswordGenerator.py
@@ -1,8 +1,8 @@
-from ast import Pass
 import random
-import sys
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 Password = []
@@ -12,24 +12,26 @@ HowManyLetters = input("How many letters would you like in your Password?: ")
 HowManySymbols = input("How many symbols would you like in your Password?: ")
 HowManyNumbers = input("How many numbers would you like in your Password?: ")
 
+
 def ListToString(list):
     string = ""
     for i in range(len(list)):
         string += list[i]
     return string
 
-for i in range(0,int(HowManyLetters)):
+
+for i in range(0, int(HowManyLetters)):
     randomics = random.choice(letters)
     Password.append(randomics)
 
-for i in range(0,int(HowManySymbols)):
+for i in range(0, int(HowManySymbols)):
     randomics = random.choice(symbols)
     Password.append(randomics)
 
-for i in range(0,int(HowManyNumbers)):
+for i in range(0, int(HowManyNumbers)):
     randomics = random.choice(numbers)
     Password.append(randomics)
-    
+
 random.shuffle(Password)
 Password_String = ListToString(Password)
 
