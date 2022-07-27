0bfc51af08a440f69c01a207afbb30b93f1e8bf3
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-07-27 15:22:31 +0430

    update

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -1,29 +1,51 @@
-from selenium import webdriver
-from bs4 import BeautifulSoup
+import requests
 import pandas as pd
+from bs4 import BeautifulSoup as Bs
+from lxml import etree
+from currency_converter import CurrencyConverter
 
 names = []
 salary = []
 costs = []
 rents = []
-driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-driver.get("https://www.numbeo.com/cost-of-living/country_result.jsp?country=Italy")
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
-for a in soup.findAll('div', href=True, attrs={'class': 'innerWidth'}):
-    name = a.find('div', attrs={'class': 'purple_light'})
-    salaries = a.find('div', attrs={'class': 'first_currency'})
-    cost = a.find('div', attrs={'class': 'in_other_currency'})
-    rent = a.find('div', attrs={'class': 'first_currency'})
-    names.append(name.text)
-    salary.append(salaries.text)
-    costs.append(cost.text)
-    rents.append(rent.text)
+countries = ["Italy", "Iran", "Norway", "Finland", 'Iraq', 'Mexico', 'France']
+base_url = "https://www.numbeo.com/cost-of-living/country_result.jsp"
+c = CurrencyConverter()
 
-df = pd.DataFrame({'City Name': names,
-                   'Salary': salary,
-                   'Cost': costs,
-                   'Rent': rents})
+for country in countries:
+    name_file = country
+    f = open("./countries/" + country + ".html", "w", encoding="utf-8")
 
-df.to_csv('products.csv', index=False, encoding='utf-8')
+    query_param = "?country="+country
+    url = base_url + query_param
+    response = requests.get(url)
+    soup = Bs(response.content, "html.parser")
+
+    f.write(str(soup))
+    f.close()
+
+    src_lxml = "./countries/" + country + ".html"
+    tree = etree.parse(src_lxml)
+
+    name_of_country = tree.xpath("//span[@class='purple_light']/text()")
+    cost = tree.xpath("//span[@class='emp_number']/text()")
+    salaries = tree.xpath("//span[@class='first_currency']/text()")
+    rent = tree.xpath("//span[@class='first_currency']/text()")
+
+    salary_final = ''.join(map(str.strip, (salaries[-2])))
+    rent_final = ''.join(map(str.strip, rent[-8]))
+
+    names.append(name_of_country[0])
+    salary.append(salary_final)
+    costs.append(cost[1])
+    rents.append(rent_final)
+
+
+df = pd.DataFrame({
+    'City Name': names,
+    'Salary': salary,
+    'Costs': costs,
+    'Rent': rents})
+
+df.to_csv('products.csv', index=False)
