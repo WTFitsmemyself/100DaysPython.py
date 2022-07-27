8d0c8b3f79f2935be0d6f7c222602cc547b23516
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2022-07-27 18:48:56 +0430

    update

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -2,22 +2,20 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup as Bs
 from lxml import etree
-from currency_converter import CurrencyConverter
 
 names = []
 salary = []
 costs = []
 rents = []
 
-countries = ["Italy", "Iran", "Norway", "Finland", 'Iraq', 'Mexico', 'France']
+countries = ["Italy", "Iran", "Norway", "Finland", 'Iraq', 'Mexico', 'France', 'China', 'Sweden']
 base_url = "https://www.numbeo.com/cost-of-living/country_result.jsp"
-c = CurrencyConverter()
 
 for country in countries:
     name_file = country
     f = open("./countries/" + country + ".html", "w", encoding="utf-8")
 
-    query_param = "?country="+country
+    query_param = "?country="+country+"&displayCurrency=USD"
     url = base_url + query_param
     response = requests.get(url)
     soup = Bs(response.content, "html.parser")
@@ -33,14 +31,25 @@ for country in countries:
     salaries = tree.xpath("//span[@class='first_currency']/text()")
     rent = tree.xpath("//span[@class='first_currency']/text()")
 
-    salary_final = ''.join(map(str.strip, (salaries[-2])))
-    rent_final = ''.join(map(str.strip, rent[-8]))
+    salary_final = list(''.join(map(str.strip, (salaries[-2]))))
+    rent_final = list(''.join(map(str.strip, rent[-8])))
+    cost_final = list(cost[1])
+
+    items = [salary_final, rent_final, cost_final]
+    for item in items:
+        if item[-1] == '$':
+            item[-1] = ''
+        elif item[-2] == '$':
+            item[-2] = ''
+
+    str_salary_final = ''.join(map(str.strip, salary_final))
+    str_rent_final = ''.join(map(str.strip, rent_final))
+    str_cost_final = ''.join(map(str.strip, cost_final))
 
     names.append(name_of_country[0])
-    salary.append(salary_final)
-    costs.append(cost[1])
-    rents.append(rent_final)
-
+    salary.append(str_salary_final)
+    costs.append(str_cost_final)
+    rents.append(str_rent_final)
 
 df = pd.DataFrame({
     'City Name': names,
