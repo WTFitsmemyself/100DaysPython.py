fe5aa6ecceb422828563c9f008357112f3900092
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2025-09-11 19:35:51 +0330

    some files updated

--- Day 1 - 10/2-Day/TipCalculator.py
+++ Day 1 - 10/2-Day/TipCalculator.py
@@ -2,6 +2,9 @@ print("Welcome to the tip calculator!")
 total_bill = float(input("What was the total bill? $"))
 number_of_people = int(input("What was the number of people to split bill with? "))
 tip_percentage = int(input("What percentage of tip you like to give(10, 12 or 15)? "))
-hundered = 100
-each_person_pay = round(((total_bill * (hundered + tip_percentage)) / hundered) / number_of_people,2)
-print("Each person should pay: $"+ str(each_person_pay))
+
+percent_bill = total_bill + ((tip_percentage/100)*total_bill)
+how_much_each = percent_bill/number_of_people
+
+print("each person should pay:" + str(round(how_much_each,3)))
+
