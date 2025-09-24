037251d727800667e419475a0790a97b717165de
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2025-09-24 20:08:02 +0330

    some files updated

--- Day 21 - 30/Day 25/main.py
+++ Day 21 - 30/Day 25/main.py
@@ -5,6 +5,8 @@
 # import csv
 import pandas
 
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
 # with open("weather_data.csv") as weather_data:
 #     data = csv.reader(weather_data)
 #     temperatures = []
@@ -14,4 +16,12 @@ import pandas
 #     print(temperatures)
 
 data = pandas.read_csv("weather_data.csv")
-print(data)
+# data_list_temp = data["temp"].to_list()
+# # print(data_list_temp)
+# avg_temp = sum(data_list_temp) / len(data_list_temp)
+# print(avg_temp)
+# data_mean = data["temp"].mean()
+# print(data_mean)
+# print(data[data["temp"] == data["temp"].max()])
+monday = data[data["day"] == "Monday"]
+print(celsius_to_fahrenheit(monday.temp))
