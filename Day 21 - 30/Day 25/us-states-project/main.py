0c06f00c5743925a7a8ba6db62b88cc23d72760c
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2025-09-25 14:20:59 +0330

    USA states project completed

--- Day 21 - 30/Day 25/us-states-project/main.py
+++ Day 21 - 30/Day 25/us-states-project/main.py
@@ -9,6 +9,7 @@ IMG_PATH = "blank_states_img.gif"
 data = pd.read_csv("./50_states.csv")
 STATES_LIST = data['state'].to_list()
 user_guesses = []
+states_to_learn = []
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -21,15 +22,20 @@ pen.hideturtle()
 
 while NUMBER_OF_STATES != NUMBER_OF_USER_GUESSES:
     ans_user_state = screen.textinput(title=f"Enter a State {NUMBER_OF_USER_GUESSES}/{NUMBER_OF_STATES}", prompt="What is another State?: ").title()
-    ans_user_x = int(data[data['state'] == ans_user_state].x)
-    ans_user_y = int(data[data['state'] == ans_user_state].y)
+    ans_user_x = data[data['state'] == ans_user_state].x
+    ans_user_y = data[data['state'] == ans_user_state].y
 
-    if ans_user_state in STATES_LIST:
+    if ans_user_state == 'Exit':
+        for state in STATES_LIST:
+            if state not in user_guesses:
+                states_to_learn.append(state)
+        break
+
+    elif ans_user_state in STATES_LIST:
         pen.penup()
-        pen.goto(ans_user_x, ans_user_y)
+        pen.goto(int(ans_user_x), int(ans_user_y))
         pen.write(f"{ans_user_state}", align="center", font=("Arial", 8, "normal"))
         NUMBER_OF_USER_GUESSES += 1
         user_guesses.append(ans_user_state)
         screen.update()
-
 screen.mainloop()
