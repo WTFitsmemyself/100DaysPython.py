2a17250fdb69edb7050e08b7f23f0c995327e03a
hosyn <77008435+WTFitsmemyself@users.noreply.github.com>
2025-10-17 18:22:05 +0330

    Day 30 Finished

--- Day 21 - 30/Day 26/Nato alphabet Project.py
+++ Day 21 - 30/Day 26/Nato alphabet Project.py
@@ -1,14 +1,19 @@
 import pandas as pd
 
 #convert name to list
-user_name = list(input("Enter your name: ").upper())
-
-#read the NATO csv file
 nato_data = pd.read_csv('nato_phonetic_alphabet.csv')
 
-#new format dict build
-new_nato_dict = {row.letter:row.code for (index, row) in nato_data.iterrows()}
+# new format dict build
+new_nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 
+def generate_nato_dict():
+    user_name = list(input("Enter your name: ").upper())
+    try:
+        new_nato_list = [new_nato_dict[name] for name in user_name]
+    except KeyError:
+        print("No such name")
+        generate_nato_dict()
+    else:
+        print(new_nato_list)
 
-new_nato_list = [new_nato_dict[name] for name in user_name if name in new_nato_dict]
-print(new_nato_list)
+generate_nato_dict()
